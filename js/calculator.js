583fcb3f2235c336d638a8acf73c99e001c9a6ea
Matt Stephens <mfstep@umich.edu>
2013-07-11 13:22:43 -0400

    Added bindings to buttons

---
+++ js/calculator.js
@@ -0,0 +1,65 @@
+(function() {
+	$("#seven").click(function() {
+		alert("seven");
+	});
+
+	$("#eight").click(function() {
+		alert("eight");
+	});
+
+	$("#nine").click(function() {
+		alert("nine");
+	});
+
+	$("#divide").click(function() {
+		alert("divide");
+	});
+
+	$("#four").click(function() {
+		alert("four");
+	});
+
+	$("#five").click(function() {
+		alert("five");
+	});
+
+	$("#six").click(function() {
+		alert("six");
+	});
+
+	$("#multiply").click(function() {
+		alert("multiply");
+	});
+
+	$("#one").click(function() {
+		alert("one");
+	});
+
+	$("#two").click(function() {
+		alert("two");
+	});
+
+	$("#three").click(function() {
+		alert("three");
+	});
+
+	$("#subtract").click(function() {
+		alert("subtract");
+	});
+
+	$("#zero").click(function() {
+		alert("zero");
+	});
+
+	$("#decimal").click(function() {
+		alert("decimal");
+	});
+
+	$("#equal").click(function() {
+		alert("equal");
+	});
+
+	$("#add").click(function() {
+		alert("add");
+	});
+})();
